a24dbafaf4719cad0694638f669d35f37cb38031
Nguyễn Văn Thông <thongnv@linxhq.com>
2015-11-10 09:33:10 +0700

    delete info database file db

--- linxbooks/protected/config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-            $dbConfig["connectionString"] = "mysql:host=localhost;dbname=linxbooks10112015";
-            $dbConfig["username"] = "root";
-            $dbConfig["password"] = "admin123";
-
-        ?>
