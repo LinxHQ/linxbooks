af1f96d8a7e4ec4c83d722ecad7a76637e1d5907
Linxbooks Admin <lbteam@linxhq.com>
2017-05-04 11:28:44 +0700

    deleted live file
    deleted

--- linxbooks/protected/config/db.live.php
+++
@@ -1,4 +0,0 @@
-<?php
-$dbConfig['connectionString'] = 'mysql:host=mysql51-081.wc2.dfw1.stabletransit.com;dbname=536091_linxbklive';
-$dbConfig['username'] = '536091_linxb';
-$dbConfig['password'] = 'Lkeaj9823$@1-0RTAR';
