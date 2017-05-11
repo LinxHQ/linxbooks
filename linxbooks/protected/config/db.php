2ce18c19ccca3cd7d18bea4f94c89a424f640dc1
Linxbooks Admin <lbteam@linxhq.com>
2017-05-11 12:39:57 +0700

    Delete db.php

--- linxbooks/protected/config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-            $dbConfig["connectionString"] = "mysql:host=localhost;dbname=536091_linxbklive";
-            $dbConfig["username"] = "root";
-            $dbConfig["password"] = "root";
-
-        ?>
