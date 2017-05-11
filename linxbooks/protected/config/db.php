e4c5e7ff022a490cbad5144425df789749ca950b
Joseph Pham Ngoc Cuong <joseph.pnc@linxhq.com>
2017-05-11 12:41:06 +0700

    updated config file

---
+++ linxbooks/protected/config/db.php
@@ -0,0 +1,6 @@
+<?php
+            $dbConfig["connectionString"] = "mysql:host=localhost;dbname=linxbooks2";
+            $dbConfig["username"] = "root";
+            $dbConfig["password"] = "root";
+
+        ?>
